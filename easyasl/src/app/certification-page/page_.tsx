93d871285dff9ee49d588a77c4083d0fe286b40a
demonicsandwich <45207659+demonicsandwich@users.noreply.github.com>
2024-10-06 08:53:04 -0400

    final stuff

---
+++ easyasl/src/app/certification-page/page_.tsx
@@ -0,0 +1,12 @@
+//for education
+import Image from "next/image";
+import React from 'react';
+import TestWord from '@/app/components/TestWord'
+
+export default function certificationpage() {
+  return (
+    <div>
+      <TestWord correctWord="want" />
+    </div>
+  );
+}
