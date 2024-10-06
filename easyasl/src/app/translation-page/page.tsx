6371d1e68d6888a605ce39450cca4cb2a4bf1542
emma-x1 <139976818+emma-x1@users.noreply.github.com>
2024-10-06 02:03:16 -0400

    .

--- easyasl/src/app/translation-page/page.tsx
+++ easyasl/src/app/translation-page/page.tsx
@@ -8,7 +8,9 @@ export default function TranslationPage() {
     <div>
       <h1>Translating </h1>
       <p>This is the content of the translation page.</p>
-      
+      const hello: React.FC = () => {
+        const [message, setMessage] = useState('Two-Way Translation')
+      }
       {/* Import and use the CameraCapture component */}
       <CaptureAndProcess/>
     </div>
