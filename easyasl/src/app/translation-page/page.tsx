6fc1366d82b717a18a70b653cf47354070a9e70f
jalen-cheng <86445739+jalen-cheng@users.noreply.github.com>
2024-10-06 08:50:22 -0400

    removed redundant features

--- easyasl/src/app/translation-page/page.tsx
+++ easyasl/src/app/translation-page/page.tsx
@@ -3,14 +3,12 @@ import React, { useState } from 'react';
 import CaptureAndProcess from '../components/CaptureAndProcess'; 
 import CameraCapture from '../components/CameraCapture';
 import texttospeech from '../components/TexttoSpeech'
-import speechtotext from '../components/SpeechToText_'
 import SpeechToText from '../components/SpeechToText_';
 
 
 export default function TranslationPage() {
   const [message, setMessage] = useState('');
 
-  <SpeechToText/>
 
   const handleASLToEnglish = () => {
     setMessage('Processing ASL to speech...');
@@ -20,7 +18,6 @@ export default function TranslationPage() {
 
   const handleTwoWayTranslation = () => {
     setMessage('Performing two-way translation...');
-    handleEnglishtoText();
     handleASLToEnglish();
   };
   
@@ -29,17 +26,14 @@ export default function TranslationPage() {
       <h1>Translating</h1>
       <p>{message}</p>
 
-            <button onClick={handleTwoWayTranslation} style={{ fontSize: '24px', padding: '15px', margin: '10px' }}>
-        Two-Way Translation
-      </button>
-
-      <button onClick={handleEnglishtoText} style={{ fontSize: '24px', padding: '15px', margin: '10px' }}>
-        English to Text
-      </button>
-
-      <button onClick={handleASLToEnglish} style={{ fontSize: '24px', padding: '15px', margin: '10px' }}>
-        ASL to Spoken English
+      <button style={{ fontSize: '24px', padding: '15px', margin: '10px' }}>
+      <SpeechToText/>
       </button>
     </div>
   );
+
+
+
+
+  
   }
