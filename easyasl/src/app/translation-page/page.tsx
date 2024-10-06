5c68a258060dbc1d209512d471bb2783f22130c0
emma-x1 <139976818+emma-x1@users.noreply.github.com>
2024-10-06 02:35:24 -0400

    .

--- easyasl/src/app/translation-page/page.tsx
+++ easyasl/src/app/translation-page/page.tsx
@@ -7,28 +7,23 @@ import texttospeech from '../components/translation'
 
 
 export default function TranslationPage() {
+  const [message, setMessage] = useState('Two-Way Translation');
+  const handleClick = () => {
+    postMessage('Translation in progress...');
+  };
+
   return (
     <div>
       <h1>Translating </h1>
       <p>This is the content of the translation page.</p>
 
-      const hello: React.FC = () => {
-        const [message, setMessage] = useState('Two-Way Translation');
-        const handleClick = () => {
-          postMessage('');
-        };
-      };
-
-      return (
         <div>
           <button onClick={handleClick}>Two-Way-Translation</button>
           <p>{message}</p>
         </div>
-      )
+      
       {/* Import and use the CameraCapture component */}
       <CaptureAndProcess/>
     </div>
   );
 }
-
-export default MyComponent;
