2ed2d0c790f8a46869708fda0c6ec198dea36545
emma-x1 <139976818+emma-x1@users.noreply.github.com>
2024-10-05 20:40:40 -0400

    pages

--- easyasl/src/app/education-page/page.tsx
+++ easyasl/src/app/education-page/page.tsx
@@ -1,3 +1,12 @@
 //for education
 import Image from "next/image";
-import React from "react";
+import React from 'react';
+
+export default function EducationPage() {
+  return (
+    <div>
+      <h1>Education</h1>
+      <p>This is the content of the education page.</p>
+    </div>
+  );
+}
