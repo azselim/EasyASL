6abfd202d94332eb99f807b5ff2ca013a7b42f56
emma-x1 <139976818+emma-x1@users.noreply.github.com>
2024-10-06 01:47:18 -0400

    .

--- easyasl/src/app/components/translation.tsx
+++ easyasl/src/app/components/translation.tsx
@@ -1,9 +1,14 @@
 import fs from "fs";
 import path from "path";
 import OpenAI from "openai";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 //audio from text
-const openai = new OpenAI();
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
 const speechFile = path.resolve("./speech.mp3");
 
@@ -24,3 +29,14 @@ async function texttospeech() {
     }
 }
 texttospeech();
+
+async function speechtotext() {
+    const transcription = await openai.audio.transcriptions.create({
+    file: fs.createReadStream("xx.mp3"), //add file
+    model: "whisper-1",
+    });
+
+    console.log(transcription.text);
+
+}
+speechtotext();
